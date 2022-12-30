5441e53225db8a9a63c32936b0d60a888ed2c3ab
Phan Tri <minhtritt01@gmail.com>
2022-12-30 11:35:29 +0700

    .

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -17,7 +17,7 @@ const Experience = () => {
     { id: 2, src: css, title: 'CSS', style: 'shadow-blue-500' },
     { id: 3, src: javascript, title: 'JavaScript', style: 'shadow-yellow-500' },
     { id: 4, src: reactImage, title: 'React', style: 'shadow-blue-600' },
-    { id: 5, src: tailwind, title: 'Taiwind', style: 'shadow-sky-400' },
+    { id: 5, src: tailwind, title: 'Tailwind', style: 'shadow-sky-400' },
     { id: 6, src: node, title: 'Node JS', style: 'shadow-green-400' },
     { id: 7, src: reactquery, title: 'React-Query', style: 'shadow-red-400' },
     { id: 8, src: github, title: 'GitHub', style: 'shadow-gray-400' },
