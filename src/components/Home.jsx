9e22679505e8489f716ba7fc5576f581da602770
Phan Tri <tri_phan@onskyhealth.com>
2024-09-06 14:55:17 +0700

    change desc

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
-import HeroImage from '../assets/heroImage.png';
-import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
-import { Link } from 'react-scroll';
-import Typed from 'typed.js';
-import React from 'react';
-import Tilt from 'react-parallax-tilt';
+import HeroImage from "../assets/heroImage.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
+import Typed from "typed.js";
+import React from "react";
+import Tilt from "react-parallax-tilt";
 
 const Home = () => {
   // Create reference to store the DOM element containing the animation
@@ -15,7 +15,6 @@ const Home = () => {
     const options = {
       strings: [
         `Hey ! I'm a <span style="color:#19A3E0;text-decoration-line: underline;">Phan Minh Tri</span>`,
-        `I'm a <span style="color:#19A3E0;text-decoration-line: underline;">Backend Developer</span>`,
         `I'm a <span style="color:#19A3E0;text-decoration-line: underline;">Frontend Developer</span>`,
       ],
       typeSpeed: 50,
@@ -33,36 +32,37 @@ const Home = () => {
   }, []);
   return (
     <div
-      name='home'
-      className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
-      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-        <div className='flex flex-col justify-center h-full flex-[2] ml-2'>
-          <h2 className='text-4xl sm:text-7xl text-white font-bold' ref={el} />
-          <p className='text-gray-400 py-2 max-w-md '>
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full flex-[2] ml-2">
+          <h2 className="text-4xl sm:text-7xl text-white font-bold" ref={el} />
+          <p className="text-gray-400 py-2 max-w-md ">
             I have learned 4 years of experience building and designing
-            software. Currently, I love to work on web application using
-            technologies like React, Tailwind, Nodejs, Express and Mongoose
+            software. I love to work on web application using technologies like
+            React, Tailwind, Nodejs, Express and Mongoose. Currently, I
+            specialize in Flutter for developing cross-platform applications.
           </p>
 
           <div>
-            <Link to={'portfolio'} smooth duration={500}>
-              <button className='group text-white rounded-md w-fit px-6 py-3 my-2 flex items-center bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+            <Link to={"portfolio"} smooth duration={500}>
+              <button className="group text-white rounded-md w-fit px-6 py-3 my-2 flex items-center bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
                 Portfolio
-                <span className='group-hover:rotate-90'>
-                  <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
+                <span className="group-hover:rotate-90">
+                  <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
                 </span>
               </button>
             </Link>
           </div>
         </div>
 
-        <div className='flex-1'>
+        <div className="flex-1">
           <Tilt>
             <img
               src={HeroImage}
-              alt='my profile'
-              className='rounded-2xl md:w-full mx-auto w-2/3'
+              alt="my profile"
+              className="rounded-2xl md:w-full mx-auto w-2/3"
             />
           </Tilt>
         </div>
