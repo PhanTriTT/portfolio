21bc54c26f092839e6976daec6291c444f341d6e
Phan Tri <tri_phan@onskyhealth.com>
2024-09-19 15:36:52 +0700

    add dart and fix resume

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,6 +12,7 @@ import formilk from "../assets/formilk.png";
 import flutter from "../assets/flutter.png";
 import bloc from "../assets/bloc.png";
 import riverpod from "../assets/riverpod.png";
+import dart from "../assets/dart.png";
 import axios from "../assets/axios.png";
 import redux from "../assets/redux.png";
 const Experience = () => {
@@ -31,6 +32,7 @@ const Experience = () => {
     { id: 13, src: flutter, title: "Flutter", style: "shadow-blue-500" },
     { id: 14, src: bloc, title: "Bloc", style: "shadow-blue-500" },
     { id: 15, src: riverpod, title: "Riverpod", style: "shadow-blue-500" },
+    { id: 16, src: dart, title: "Riverpod", style: "shadow-green-300" },
   ];
   return (
     <div
